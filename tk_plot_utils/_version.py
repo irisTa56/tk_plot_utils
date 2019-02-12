c69f2d68a3d0a4c22a381032b61affe12e5f951b
Takayuki Kobayashi <iris.takayuki@gmail.com>
2019-02-12 15:21:33 +0900

    update version

--- tk_plot_utils/_version.py
+++ tk_plot_utils/_version.py
@@ -4,5 +4,5 @@
 # Copyright (c) Takayuki Kobayashi.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 7, 3, "dev")
+version_info = (0, 7, 4, "dev")
 __version__ = ".".join(map(str, version_info))
