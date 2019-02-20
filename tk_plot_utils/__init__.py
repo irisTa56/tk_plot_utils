cae18d480a0cc89bf13eae6173638824214d1c99
Takayuki Kobayashi <iris.takayuki@gmail.com>
2019-02-20 15:42:02 +0900

    add __version__ attribute

--- tk_plot_utils/__init__.py
+++ tk_plot_utils/__init__.py
@@ -1,5 +1,7 @@
 """An interface to Plotly"""
 
+from ._version import __version__
+
 from .plotly_html import init_plotly
 from .plotly_traces import make_scatter, make_heatmap
 from .plotly_utils import tools
@@ -8,6 +10,7 @@ from .plotly_utils import pltgo as go
 from .plotly_utils import ExtendedFigureWidget as plotly
 
 __all__ = [
+  "__version__",
   "pl",
   "go",
   "plotly",
